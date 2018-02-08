913188545a4312b847ca694be3d419b48e3d1c83
Chu Qing Hao <633703+qinghao1@users.noreply.github.com>
2018-02-08 14:34:46 +0800

    Fix test

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -13,7 +13,7 @@ import seedu.addressbook.data.person.address.PostalCode;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
+    public static final String EXAMPLE = "123, Clementi Ave 3, #12-34, 231534";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses should be in the format of: " +
             "'a/BLOCK, STREET, UNIT, POSTAL_CODE'";
     public static final String ADDRESS_VALIDATION_REGEX = "(.+),(.+),(.+),(.+)";
